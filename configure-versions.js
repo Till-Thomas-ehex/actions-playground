24f3627e526140ae3418da99094148dc861b58ac
Till Thomas <Till.Thomas@ehex.de>
2025-01-10 11:10:54 +0100

    add configure-versions script

---
+++ configure-versions.js
@@ -0,0 +1,40 @@
+const spawn = require("cross-spawn");
+const semver = require("semver");
+
+const result = spawn.sync("git describe --exact-match --tags");
+const version = result.stdout.toString("utf8");
+console.log(version);
+console.error(result.stderr.toString("utf8"));
+
+console.log(semver.parse(version));
+console.log(createManifestVersions(semver.parse(version)), "string");
+
+function createManifestVersions(version) {
+  const isTag = version.toString().split("-").length - 1 < 2;
+
+  if (isTag) {
+    console.log("isTag");
+
+    return {
+      rustManifest: version.toString(),
+      tauriManifest: `${version.major}.${version.minor}.${version.patch}`, // for cross-platform compatibility, no suffix
+    };
+  } else if (
+    ["RC", "alpha", "beta"].some((v) => version.prerelease.includes(v))
+  ) {
+    console.log("RC alpha beta");
+
+    return {
+      rustManifest: `${version.major}.${version.minor}.${version.patch}-SNAPSHOT`,
+      tauriManifest: `${version.major}.${version.minor}.${version.patch}`, // for cross-platform compatibility, no suffix
+    };
+  } else {
+    console.log("else");
+
+    const nextMinor = version.minor + 1;
+    return {
+      rustManifest: `${version.major}.${nextMinor}.0-SNAPSHOT`,
+      tauriManifest: `${version.major}.${nextMinor}.0`, // for cross-platform compatibility, no suffix
+    };
+  }
+}
