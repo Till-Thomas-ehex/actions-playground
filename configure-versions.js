17e614fb0f0080d36d4845f33286a9e6407e984e
Till Thomas <Till.Thomas@ehex.de>
2025-01-10 13:37:02 +0100

    add mock toml and json

--- configure-versions.js
+++ configure-versions.js
@@ -18,8 +18,6 @@ function createManifestVersions(version) {
   const isTag = version.toString().split("-").length - 1 < 2;
 
   if (isTag) {
-    console.log("isTag");
-
     return {
       rustManifest: version.toString(),
       tauriManifest: `${version.major}.${version.minor}.${version.patch}`, // for cross-platform compatibility, no suffix
@@ -27,15 +25,11 @@ function createManifestVersions(version) {
   } else if (
     ["RC", "alpha", "beta"].some((v) => version.prerelease.includes(v))
   ) {
-    console.log("RC alpha beta");
-
     return {
       rustManifest: `${version.major}.${version.minor}.${version.patch}-SNAPSHOT`,
       tauriManifest: `${version.major}.${version.minor}.${version.patch}`, // for cross-platform compatibility, no suffix
     };
   } else {
-    console.log("else");
-
     const nextMinor = version.minor + 1;
     return {
       rustManifest: `${version.major}.${nextMinor}.0-SNAPSHOT`,
